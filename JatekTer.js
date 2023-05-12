9e4b2e3893d1839f9efe5d4602ed7c31958849f8
„SnowGRH” <gergelyovinczeffy@gmail.com>
2023-05-12 10:57:30 +0200

    szin változtatás nem megy

--- JatekTer.js
+++ JatekTer.js
@@ -2,20 +2,30 @@ import Lampa from "./lampa.js"
 
 class JatekTer{
     #db;
-    #allapotLista=[];
-    #meret;
+    #allapotLista=[true,false,true];
+    #meret = 7;
     #lepes;
     
     constructor(){
         const ART = $("article");
-        let allapotLista = this.#allapotLista;
-        for (let index = 0; index < 7; index++) {
-            const lampa = new Lampa(index,allapotLista[index],ART);
+        let meret = this.#meret;
+        let index = 0;
+        for (index = 0; index <= meret; index++) {
+            const lampa = new Lampa(index,this.#allapotLista[index],ART);
             console.log(index);
         }
-    }
-    #setAllapotLista(){
+        $(window).on("kattintasTrigger",(event)=>{
+            console.log(event.detail)
+            if (this.#allapotLista[event.detail.id] === true) {
+                console.log("hello");
 
+            }
+
+            
+        })
+    }
+    #setAllapotLista(id,ertek){
+        this.#allapotLista[id] =ertek;
     }
     #szomszedokKeresese(id){
         
--- Lampa.js
+++ Lampa.js
@@ -5,20 +5,29 @@ class Lampa{
 
     constructor(id, allapot, szuloElem){
         this.#id=id;
-        this.#divElem = `<div class="lampa"><p class="alap" id="${id}"></p></div>`;
+        this.#allapot = allapot;   
+        this.#divElem = `<div style="background-color: red" class="lampa col-4 col-sm-4 col-md-4 col-lg-4 col-xl-4 col-xxl-4" id="${id}"></div>`;
         szuloElem.append(this.#divElem);
         this.divElem = $(".lampa:last-child");
         this.pElem = this.divElem.children("p");
-        this.divElem
+        this.divElem.on("click",()=>{
+            this.setAllapot(this.#allapot);
+            this.#kattintasTrigger();
+          })
     }
-    setAllapot(){
-
+    setAllapot(allapot){
+        if (allapot === true) {
+            this.#szinBeallit("red");
+        } else {
+            
+        }
     }
-    szinBeallit(){
-
+    #szinBeallit(ertek){
+    this.divElem.css("background-color:",ertek);
     }
-    kattintasTrigger(){
-
+    #kattintasTrigger(){
+        const LAMPA = new CustomEvent("kattintasTrigger", { detail: this });
+        window.dispatchEvent(LAMPA);
     }
 }
 export default Lampa
