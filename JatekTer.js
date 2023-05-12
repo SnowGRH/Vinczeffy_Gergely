8df0e0fc9961bb7c1ad2d5f19a4eaba6a48fecc3
„SnowGRH” <gergelyovinczeffy@gmail.com>
2023-05-12 09:37:57 +0200

    Tervrajz alapján

---
+++ JatekTer.js
@@ -0,0 +1,21 @@
+class JatekTer{
+    #db;
+    #allapotLista;
+    #meret;
+    #lepes;
+    
+    constructor(){
+
+    }
+    setAllapotLista(){
+
+    }
+    init(){
+
+    }
+    ellenorzes(){
+
+    }
+
+}
+export default JatekTer
---
+++ Lampa.js
@@ -0,0 +1,19 @@
+class Lampa{
+    #allapot;
+    #id;
+    #divElem;
+
+    constructor(id, allapot, divElem){
+
+
+    }
+    setAllapot(){
+
+    }
+    szinBeallit(){
+
+    }
+    kattintasTrigger(){
+        
+    }
+}
